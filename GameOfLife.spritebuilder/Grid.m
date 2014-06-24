e1c8aa6621e4c1045ad8e83afcfa42dee8ee00a2
JoshuaSani <joshuasani@Joshuas-MacBook-Pro.local>
2014-06-24 18:11:31 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -77,6 +77,13 @@ static const int GRID_COLUMNS = 10;
     
     // invert it's state - kill it if it's alive, bring it to life if it's dead.
     creature.isAlive = !creature.isAlive;
+    
+    if (creature.isAlive == TRUE)
+    {
+        _totalAlive++;
+    }
+    else
+        _totalAlive--;
 }
 
 - (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
